72582d0c435af2f77bee375807b4f8756dbff54b
Miguel Gonzalez <lolosumine@gmail.com>
2023-05-20 09:01:45 -0500

    eureka

--- ServicioRuta/src/main/java/com/proyect/service/ServicioRutaApplication.java
+++ ServicioRuta/src/main/java/com/proyect/service/ServicioRutaApplication.java
@@ -2,8 +2,10 @@ package com.proyect.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServicioRutaApplication {
 
 	public static void main(String[] args) {
