02d9149a698dd3b39e3bfa6ca0d0bb33f43f498d
Miguel Gonzalez <lolosumine@gmail.com>
2023-08-01 16:07:17 -0500

    gateway

---
+++ ServicioConfig/src/main/java/com/config/service/controller/ParadaController.java
@@ -0,0 +1,23 @@
+package com.config.service.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+public class ParadaController {
+
+	@GetMapping("/nia")
+	public String saludar() {
+		return "Miaow";
+	}
+}
