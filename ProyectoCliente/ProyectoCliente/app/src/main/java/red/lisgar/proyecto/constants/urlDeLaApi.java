c628aae75a59d7b4825a9102cb4fc96573d9a910
Miguel Gonzalez <lolosumine@gmail.com>
2023-08-05 11:05:46 -0500

    gateway

--- ProyectoCliente/ProyectoCliente/app/src/main/java/red/lisgar/proyecto/constants/urlDeLaApi.java
+++ ProyectoCliente/ProyectoCliente/app/src/main/java/red/lisgar/proyecto/constants/urlDeLaApi.java
@@ -2,6 +2,6 @@ package red.lisgar.proyecto.constants;
 
 public class urlDeLaApi {
 
-    public static final String URL = "http://192.168.1.22:8080/user/";
+    public static final String URL = "http://10.0.2.2:8080/user/";
 
 }
