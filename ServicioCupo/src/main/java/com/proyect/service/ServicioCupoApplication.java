c241851697a80cf2b2e7e66a2bc5bfa1cdb5696b
Miguel Gonzalez <lolosumine@gmail.com>
2023-08-20 12:43:24 -0500

    Cinco

--- ServicioCupo/src/main/java/com/proyect/service/ServicioCupoApplication.java
+++ ServicioCupo/src/main/java/com/proyect/service/ServicioCupoApplication.java
@@ -2,10 +2,8 @@ package com.proyect.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ServicioCupoApplication {
 
 	public static void main(String[] args) {
